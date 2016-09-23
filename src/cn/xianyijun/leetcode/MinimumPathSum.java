08f6f29c91db9564ba8c75d09c2d9dac722d359a
xianyijun <xianyijun@creatshare.com>
2016-09-23 19:37:10 +0800

    ac the MinimumPathSum

---
+++ src/cn/xianyijun/leetcode/MinimumPathSum.java
@@ -0,0 +1,33 @@
+package cn.xianyijun.leetcode;
+
+/**
+ * @author xianyijun
+ *Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+ */
+public class MinimumPathSum {
+	public  int minPathSum(int[][] grid) {
+		if (grid == null || grid.length == 0) {
+			return 0;
+		}
+		int[][] result = new int[grid.length][grid[0].length];
+		result[0][0] = grid[0][0];
+		for (int i = 1; i < grid.length; i++) {
+			result[i][0] = result[i - 1][0] + grid[i][0];
+		}
+		for (int i = 1; i < grid[0].length; i++) {
+			result[0][i] = result[0][i - 1] + grid[0][i];
+		}
+		for (int i = 1; i < grid.length; i++) {
+			for (int j = 1; j < grid[0].length; j++) {
+				result[i][j] = Math.min(result[i - 1][j], result[i][j - 1]) + grid[i][j];
+			}
+		}
+		return result[grid.length - 1][grid[0].length - 1];
+	}
+
+	public static void main(String[] args) {
+		System.out.println(minPathSum(new int[][] { { 1, 2, 5 }, { 3, 2, 1 } }));
+	}
+}
